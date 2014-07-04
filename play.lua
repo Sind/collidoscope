042d9355a6f903ec2b285823cf7f5b1d87ee3b05
Amadiro <jwringstad@gmail.com>
2014-07-04 16:18:10 +0200

    added scheme handling

--- play.lua
+++ play.lua
@@ -23,8 +23,23 @@ field = {
 }
 
 function play.load()
-	play.images = {}
-	play.images.background = love.graphics.newImage("assets/board-design.png")
+	-- play.load()
+	-- load initial assets, initialize schemes we're going to use.
+
+	play.scheme = {}
+	play._load_scheme('clairdelune')
+end
+function play._load_scheme(name)
+	-- play._load_scheme
+	-- internal function for loading a scheme.
+	-- schemes are mostly just colors, but can contain some extra
+	-- images for backgrounds etc.
+	
+	print("Loading scheme: '" .. name .. "'")
+	play.scheme[name] = {}
+	play.scheme[name].basepath = "assets/schemes/" .. name .. "/"
+	play.scheme[name].background = love.graphics.newImage(play.scheme[name].basepath .. "background.png")
+
 end
 
 function play.update(dt)
@@ -44,11 +59,16 @@ function play.draw(debug_draw)
 	if debug_draw then
 		play._debug_draw()
 	else
-		play._do_draw()
+		play._do_draw('clairdelune')
 	end
 
 end
 
-function play._do_draw()
-	love.graphics.draw(play.images.background)
+function play._do_draw(scheme_name)
+	-- extract the scheme "folder" table
+	scheme = play.scheme[scheme_name]
+	
+	-- draw background
+	love.graphics.draw(scheme.background)
+	-- draw decorations
 end
