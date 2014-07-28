ba686da0e79fcc87816bfdda8b68c19f873a8c64
Jonathan Ringstad <jwringstad@gmail.com>
2014-07-28 18:25:00 +0200

    changed keybinding for p1 block stomp to tab

--- binding.lua
+++ binding.lua
@@ -1,7 +1,7 @@
 binding = {}
 binding.pressed = {}
 binding.isDown = {}
-binding.map = {p1h = "q",p1s = "e", p1u = "w", p1d = "s", p1l = "a", p1r= "d", p2h = "rshift",p2s = "rctrl",p2u = "up", p2d = "down", p2l = "left", p2r= "right",start="return"}
+binding.map = {p1h = "q",p1s = "tab", p1u = "w", p1d = "s", p1l = "a", p1r= "d", p2h = "rshift",p2s = "rctrl",p2u = "up", p2d = "down", p2l = "left", p2r= "right",start="return"}
 function binding.update()
 	for k,v in pairs(binding.map) do
 		binding.pressed[k] = love.keyboard.isDown(binding.map[k]) and not binding.isDown[k]
